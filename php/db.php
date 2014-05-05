d08355a5f2b80cf5e208c5fca4c372e11b58a69a
ada <adalgiso@users.noreply.github.com>
2014-05-05 14:28:14 -0400

    Delete db.php

--- php/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$username = "iwuml74";
-$pw = "m8fuo7";
-$database = "DCIT2804_FOOTPRINTSADMIN";
-$server = "bark.clemson.edu";
-
-mysql_connect($server,$username,$pw);
-@mysql_select_db($database) or die("Unable to select database.\n");
-?>
